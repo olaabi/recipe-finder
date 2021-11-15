97c7b67a0f5748a128445aab3b687595db83a1d1
Capt0bvious <abiola.olaniyan15@gmail.com>
2021-11-14 23:42:17 -0500

    first commit; added file for testing OpenCV

---
+++ test.py
@@ -0,0 +1,12 @@
+import cv2
+
+cam = cv2.VideoCapture(0)
+while True:
+    ret, frame = cam.read()
+    cv2.imshow('test', frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows
